a7854460ba6d82b427becaa3e27b725cc9886a35
celapeterson <56178722+celapeterson@users.noreply.github.com>
2020-10-03 11:16:54 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+Cecelia Peterson: cpeterson36@wisc.edu, 9073157274
+Kelsey Hickok: khickok@wisc.edu, 9076435016
+*/
+
+ /*
+ The main function calls functions to print requested fields for given process ids
+ Input: command line args
+ Return: int representing exit on success or failure
+ */
+ int main(int argC, char *argV[]) {
+ 
+   return 0;
+ 
+ }
