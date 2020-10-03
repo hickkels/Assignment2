945291b7b881cb2cd357e0a6055e45933ec0354f
celapeterson <56178722+celapeterson@users.noreply.github.com>
2020-10-03 11:41:10 -0500

    Update main.c

--- main.c
+++ main.c
@@ -10,6 +10,29 @@ Kelsey Hickok: khickok@wisc.edu, 9076435016
  */
  int main(int argC, char *argV[]) {
  
+   // first create three queues, will return a pointer to the queue
+  
+   // then create 4 pthreads using pthread_create
+   // each has its own id
+   pthread_t Reader;
+   pthread_t Munch1; // use index!
+   pthread_t Munch2; // use islower and toupper!
+   pthread_t Writer; // when no more string to process, print # strings processed to stdout
+  
+   // thread Reader will read in each input line from stdin
+   // must check that the input line does not exceed the size of your buffer
+   // it it does: reject that line by 
+   // (1) printing out an error message to stderr and 
+   // (2) throw away (flush to end of line) any remaining characters on that line
+   
+   // wait for these threads to finish by calling pthread_join
+   // each thread will call pthread_exit
+  
+   // threads should terminate when there is no more input (end of file) TRICKY
+   
+  
+   // for each queue, print statistics to stderr uding PrintQueueStats function
+  
+  
    return 0;
- 
  }
